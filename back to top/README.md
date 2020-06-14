52e6ce0aa28a2e70baebc16d331df6f516a87bf9
Deepanshu Prajapati <dipanshuraz2@gmail.com>
2020-06-14 22:17:00 +0530

    back to top

---
+++ back to top/README.md
@@ -0,0 +1,5 @@
+## React Scroll Arrow
+
+This is the code for making your own  react scroll arrow that is hidden until you reach a certain scroll length. 
+
+![](scroll-arrow.gif)
---
+++ back to top/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ScrollTopArrow from './components/ScrollTopArrow';
+
+function App() {
+  return (
+    <div className="App">
+      <ScrollTopArrow/>
+    </div>
+  );
+}
+
+export default App;
